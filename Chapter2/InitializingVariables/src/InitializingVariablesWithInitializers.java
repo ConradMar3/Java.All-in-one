b34328890f8228f8f83083430591127205ba9d3b
ConradMar3 <96224782+ConradMar3@users.noreply.github.com>
2022-04-08 13:57:21 +0200

    2022/04/07
    Signed-off-by: ConradMar3 <96224782+ConradMar3@users.noreply.github.com>

---
+++ Chapter2/InitializingVariables/src/InitializingVariablesWithInitializers.java
@@ -0,0 +1,13 @@
+public class InitializingVariablesWithInitializers {
+    public static void main(String[] args) {
+        int x, y;
+        String lastName = "Lowe";
+        double radius = 15.4;
+        y = 5;
+        x = 10;
+
+        System.out.println(x + " " + y + " " + lastName + " " + radius);
+
+
+    }
+}
---
+++ Chapter2/PrimitiveDataTypes.java
@@ -0,0 +1,5 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+
+    }
+}
