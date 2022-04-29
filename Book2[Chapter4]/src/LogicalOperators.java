b4464fef6819e92a6b59b7ec9cbd25cb78cfa6b4
ConradMar3 <96224782+ConradMar3@users.noreply.github.com>
2022-04-29 14:12:38 +0200

    2022/04/29 (pm)
    Chapter4 - LogicalOperators
    
    Signed-off-by: ConradMar3 <96224782+ConradMar3@users.noreply.github.com>

---
+++ Book2[Chapter4]/src/LogicalOperators.java
@@ -0,0 +1,12 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        int i = 4;
+
+        if (!(i == 4))
+            System.out.println("false");
+        {
+            if (!(i != 4)) ;
+            System.out.println("true");
+        }
+    }
+}
